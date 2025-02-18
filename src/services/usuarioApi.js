370e019ea9a3772243853614930a7d3bdb908bac
Walter Fonseca <walterfonseca1606@gmail.com>
2025-02-18 17:20:37 -0300

    Iniciando meu front do projeto ApiJavaScript

---
+++ src/services/usuarioApi.js
@@ -0,0 +1,55 @@
+import api from './api';
+
+const LojaService = {
+
+  async obterUsuarioPorIdAsync(usuarioId) {
+    const response = await api.get(`/usuario/obter/${usuarioId}`);
+    return response.data;
+  },
+
+  async listarUsuariosAsync(ativo){
+    const response = await api.get(`/usuario/listar?ativo=${ativo}`);
+    return response.data;
+  },
+  
+  async logarUsuarioAsync(email, senha){
+    const usuarioLogin = {
+        email,
+        senha
+    }
+    const response = await api.get(`/usuario/logar`, usuarioLogin);
+    return response.data;
+  },
+
+  async obterUsuarioPorIdComInformacaoLojaAsync(usuarioId){
+    const response = await api.get(`/usuario/obterComLoja/${usuarioId}`);
+    return response.data;
+  },
+
+  async cadastrarUsuarioAsync(nome, email, senha){
+    const usuarioCadastro = {
+        nome,
+        email,
+        senha
+    }
+    const response = await api.post(`/usuario/cadastrar`, usuarioCadastro);
+    return response.data;
+  },
+
+  async atualizarNomeUsuarioAsync(usuarioId, nome){
+    const usuarioEditar = {
+        nome
+    }
+    const response = await api.put(`/usuario/atualizar/${usuarioId}`, usuarioEditar);
+    return response.data;
+  },
+
+  async deletarUsuarioAsync(usuarioId){
+   
+    const response = await api.put(`/usuario/deletar/${usuarioId}`);
+    return response.data;
+  },
+
+};
+
+export default LojaService;
